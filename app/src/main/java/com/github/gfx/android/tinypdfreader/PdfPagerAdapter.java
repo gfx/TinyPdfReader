a7d9cbccb567b49b83f51acf6124ad19a07ea270
FUJI Goro (gfx) <gfuji@cpan.org>
2016-04-13 09:02:01 +0900

    async bitmap loading

--- app/src/main/java/com/github/gfx/android/tinypdfreader/PdfPagerAdapter.java
+++ app/src/main/java/com/github/gfx/android/tinypdfreader/PdfPagerAdapter.java
@@ -3,14 +3,18 @@ package com.github.gfx.android.tinypdfreader;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.pdf.PdfRenderer;
+import android.os.AsyncTask;
 import android.support.v4.view.PagerAdapter;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import uk.co.senab.photoview.PhotoView;
 
 public class PdfPagerAdapter extends PagerAdapter {
+
+    static final String TAG = PdfPagerAdapter.class.getSimpleName();
 
     final Context context;
 
@@ -30,23 +34,18 @@ public class PdfPagerAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int position) {
         long t0 = System.currentTimeMillis();
 
-        PhotoView view = new PhotoView(context);
+        PhotoView imageView = new PhotoView(context);
 
-        try(PdfRenderer.Page page = pdfRenderer.openPage(position)) {
-            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
-            );
-            view.setLayoutParams(params);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        imageView.setLayoutParams(params);
+        container.addView(imageView);
 
-            Bitmap bitmap = Bitmap.createBitmap(page.getWidth() * 2, page.getHeight() * 2, Bitmap.Config.ARGB_8888);
-            page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
-            view.setImageBitmap(bitmap);
-        }
+        new LoadBitmapTask(pdfRenderer, imageView).execute(position);
 
-        container.addView(view);
-
-        Log.d("PdfPagerAdapter", "instantiateItem: " + position + " in " + (System.currentTimeMillis() - t0) + "ms");
-        return view;
+        Log.d(TAG, "instantiateItem: " + position + " in " + (System.currentTimeMillis() - t0) + "ms");
+        return imageView;
     }
 
     @Override
@@ -57,5 +56,33 @@ public class PdfPagerAdapter extends PagerAdapter {
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((View)object);
+    }
+
+    private static class LoadBitmapTask extends AsyncTask<Integer, Void, Bitmap> {
+
+        final PdfRenderer pdfRenderer;
+
+        final ImageView targetView;
+
+        public LoadBitmapTask(PdfRenderer pdfRenderer, ImageView targetView) {
+            this.pdfRenderer = pdfRenderer;
+            this.targetView = targetView;
+        }
+
+        @Override
+        protected Bitmap doInBackground(Integer... params) {
+            int position = params[0];
+
+            try(PdfRenderer.Page page = pdfRenderer.openPage(position)) {
+                Bitmap bitmap = Bitmap.createBitmap(page.getWidth() * 2, page.getHeight() * 2, Bitmap.Config.ARGB_8888);
+                page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+                return bitmap;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            targetView.setImageBitmap(bitmap);
+        }
     }
 }
